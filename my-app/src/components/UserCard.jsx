fe397c058ac7d215a1fc33df23a484c4653a8d58
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2023-11-04 00:17:58 +0530

    Added-dropdown

--- my-app/src/components/UserCard.jsx
+++ my-app/src/components/UserCard.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/userCard.css";
 export const UserCard = (obj) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleDropDown = () => {
+    setShowDropdown((pre) => !pre);
+  };
   return (
     <div id="userCard">
       <div id="userDetails">
@@ -15,10 +19,51 @@ export const UserCard = (obj) => {
         </div>
         <div>
           <h5>STATE</h5>
-          <div>{obj.address.state || "-"}</div>
+          <div>{obj.address.state || "Not Available"}</div>
         </div>
-        <button>View Details</button>
+        <button onClick={handleDropDown}>
+          {showDropdown ? "Hide Details" : "View Details"}
+        </button>
       </div>
+      {showDropdown && (
+        <div id="dropdown">
+          <h5>Description</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum velit
+            incidunt, excepturi at vel obcaecati ad reprehenderit accusantium
+            soluta dolorum magni similique autem blanditiis eius quod est nobis
+            consectetur sint assumenda laboriosam. Sit harum, alias quasi earum
+            id optio rerum facilis quaerat iste! Eum quis, earum corporis
+            deleniti veritatis repellat?
+          </p>
+          <div className="contactDetails">
+            <div>
+              <h5>Contact Person</h5>
+              <p>{obj.name || "-"}</p>
+              <h5>Designation</h5>
+              <p>{obj.designation || "-"}</p>
+              <h5>Email</h5>
+              <p>{obj.email || "-"}</p>
+              <h5>Phone</h5>
+              <p>{obj.phone || "-"}</p>
+            </div>
+            <div>
+              <h5>Address</h5>
+              <p>{`${obj.address.suite || "House No."} ${
+                obj.address.street || "Street"
+              } ${obj.address.city || "City"} ${obj.address.state || "State"} ${
+                obj.address.zipcode || "123456"
+              }`}</p>
+              <h5>City</h5>
+              <p>{obj.address.city || "-"}</p>
+              <h5>State</h5>
+              <p>{obj.address.state || "-"}</p>
+              <h5>Country</h5>
+              <p>{obj.address.country || "-"}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
